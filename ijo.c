218f1892f18cfd55eb5607bf25468497802ee258
OldoHeld11 <xheldo00@stud.fit.vutbr.cz>
2016-10-03 19:53:07 +0200

    oldo

--- ijo.c
+++ ijo.c
@@ -1,10 +1,14 @@
+//
+// Created by oldo on 3.10.16.
+//
+
 #include <stdlib.h>
 
 #include <stdio.h>
 
 int main() {
 
-    printf("Hello, World hgj g!");
+    printf("Hello, World g!");
 
     return 0;
 }
