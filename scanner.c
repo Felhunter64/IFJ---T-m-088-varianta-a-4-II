a565ff91f8cbfce97cfa805e50b51f86708e2101
OldoHeld11 <xheldo00@stud.fit.vutbr.cz>
2016-10-05 14:36:25 +0200

    oldo

---
+++ scanner.c
@@ -0,0 +1,7 @@
+//
+// Created by oldo on 5.10.16.
+//
+
+#include "scanner.h"
+#include <ctype.h>
+#include <stdio.h>
---
+++ scanner.h
@@ -0,0 +1,8 @@
+//
+// Created by oldo on 5.10.16.
+//
+
+#ifndef IFJ_88_SCANNER_H
+#define IFJ_88_SCANNER_H
+
+#endif //IFJ_88_SCANNER_H
