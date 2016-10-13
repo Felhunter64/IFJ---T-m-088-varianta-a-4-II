8d184c4fd595e1438e2e31f7f835bc2bb47b4cfe
felhunter64 <ssulaj.martin@gmail.com>
2016-10-14 01:31:54 +0200

    mato

--- memory.c
+++ memory.c
@@ -8,21 +8,91 @@
 #include "memory.h"
 
 
-typedef struct{
-    struct sAdresa **pole;
-    int size;
-}tTabulkaAdries;
 
-typedef struct sAdresa{
-    int adresa;
-    struct sAdresa *dalsiaAdresa;
-}tAdresa;
+tTableOfAddress* createHtable(int size){
 
-tTabulkaAdries createHtable(int size){
+    tTableOfAddress *htable;
 
-    tTabulkaAdries htable;
-
-    if((htable = (tTabulkaAdries) malloc(sizeof(tTabulkaAdries))) == NULL)
+    if((htable = malloc(sizeof(tTableOfAddress))) == NULL)
         return NULL;
 
+    if((htable->arr = malloc(sizeof(tAddress*) * size)) == NULL)
+        return NULL;
+
+    for(int i = 0; i < size; i++)
+        htable->arr[i] = NULL;
+
+    htable->size = size;
+
+    return htable;
+}
+
+tAddress* insertAddress(int addressNum, tTableOfAddress *htable){
+    tAddress* newAddress;
+    int hashKey = addressNum % htable->size;
+
+    if((newAddress = malloc(sizeof(tAddress))) == NULL)
+        return NULL;
+
+    newAddress->address = addressNum;
+    newAddress->nextAddress = NULL;
+
+    if(htable->arr[hashKey] == NULL){
+        htable->arr[hashKey] = newAddress;
+        return newAddress;
+    }
+
+    tAddress *tmpAddress = htable->arr[hashKey];
+
+    while(tmpAddress->nextAddress != NULL)
+        tmpAddress = tmpAddress->nextAddress;
+
+    tmpAddress->nextAddress = newAddress;
+
+    return newAddress;
+}
+
+
+//odstrani tAdresu z hesovacej tabulky a vrati jej adresu
+tAddress* retrieveAddress(int addressNum, tTableOfAddress *htable){
+    int hashKey = addressNum % htable->size;
+
+    if(htable->arr[hashKey] == NULL){
+        return NULL;
+    }
+
+    tAddress *tmpAddress = htable->arr[hashKey];
+
+    if(tmpAddress->address == addressNum){
+        htable->arr[hashKey] = tmpAddress->nextAddress;
+        free(tmpAddress);
+    }
+
+    if(tmpAddress->nextAddress == NULL)
+        return NULL;
+
+    while(tmpAddress->nextAddress->address != addressNum) {
+        tmpAddress = tmpAddress->nextAddress;
+        if(tmpAddress->nextAddress == NULL)
+            return NULL;
+    }
+
+    tAddress *tmp2Address = tmpAddress->nextAddress;
+    tmpAddress = tmpAddress->nextAddress->nextAddress;
+
+    return tmp2Address;
+}
+
+void deleteHtable(tTableOfAddress *htable){
+    tAddress* tmpAddress;
+
+    for(int i = 0; i < htable->size; i++){
+        while(htable->arr[i] != NULL){
+            tmpAddress = htable->arr[i]->nextAddress;
+            free(htable->arr[i]);
+            htable->arr[i] = tmpAddress;
+        }
+    }
+
+    return;
 }
--- memory.h
+++ memory.h
@@ -5,7 +5,15 @@
 #ifndef IFJ_88_GARBAGE_COLLECTOR_H
 #define IFJ_88_GARBAGE_COLLECTOR_H
 
+typedef struct{
+    struct sAddress **arr;
+    int size;
+}tTableOfAddress;
 
+typedef struct sAddress{
+    int address;
+    struct sAddress *nextAddress;
+}tAddress;
 
 
 
