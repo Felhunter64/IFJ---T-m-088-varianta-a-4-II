311e8c759e4a632e58d7f6d8f20e8b03b430aa7e
felhunter64 <ssulaj.martin@gmail.com>
2016-10-14 16:20:14 +0200

    mato

--- main.c
+++ main.c
@@ -4,12 +4,19 @@
 
 #include "vyrazy.h"
 #include "scanner.h"
+#include "memory.h"
+#include "errors.h"
 
 int main() {
 
+    tTableOfAddress * htable;
 
+    if((htable = createHtable(100)) == NULL)
+        return printErrors(INTERNA_CHYBA);
 
-    printf("Hello, World asdasfssgfsafasfaq g!");
+    //printf("Hello, World asdasfssgfsafasfaq g!");
+
+    deleteHtable(htable);
 
     return 0;
 
