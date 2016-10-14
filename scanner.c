84b98a1e602f9c80eb9f8adeaf7d28399b02198c
OldoHeld11 <xheldo00@stud.fit.vutbr.cz>
2016-10-14 17:15:24 +0200

    oldo

--- scanner.c
+++ scanner.c
@@ -2,14 +2,36 @@
 // Created by oldo on 5.10.16.
 //
 //todo funkcia na prevod stringu na cislo pre EDKA
+//todo ked dostanem EOF tak freeujem string ....
+//todo namallocovanie stringu
+
+#define dlzka_tokenu=50
+
+
 #include "scanner.h"
 #include <ctype.h>
 #include <stdio.h>
+#include "memory.h"
+#include "errors.h"
 
-int main(){
-    TvojaPrvaFunkcia();
+FILE *sourceFile;
+int main(int argc,char *argv[]){
+    if((sourceFile = fopen(argv, "r")) == NULL)
+        return printErrors(INTERNA_CHYBA);
+
+
+
+    tTableOfAddress * htable;
+
+    if((htable = createHtable(100)) == NULL)
+        return printErrors(INTERNA_CHYBA);
+
+
+
+    xMalloc(sizeof(struct sToken),htable);
+
+    getNextToken();
+
+    deleteHtable(htable);
 }
 
-TvojaPrvaFunkcia(){
-    printf("ahojOldo\n");
-}
--- scanner.h
+++ scanner.h
@@ -10,9 +10,61 @@ typedef struct sToken {
     int numToken;
 }tToken;
 
+typedef enum {
+    KEY_BOOLEAN=10,
+    KEY_BREAK,
+    KEY_CLASS,
+    KEY_CONTINUE,
+    KEY_DO,
+    KEY_DOUBLE,
+    KEY_ELSE,
+    KEY_FALSE,
+    KEY_FOR,
+    KEY_IF,
+    KEY_INT,
+    KEY_RETURN,
+    KEY_STRING,
+    KEY_STATIC,
+    KEY_TRUE,
+    KEY_VOID,
+    KEY_WHILE
+}keywords;
 
+typedef enum{
+    IDENT1=0,     //Jednoduchý identifikátor
+    IDENT2,     //Plně kvalifikovaný identifikátor
+}ident;
 
-getNextToken();
+typedef enum{
+    PLUS = 30,               // +
+    MINUS,              // -
+    KRAT,               // *
+    DELENO,             // /
+    MENSITKO,           // <
+    VACSITKO,           // >
+    MENSITKO_ROVNASA,   // <=
+    VACSITKO_ROVNASA,   // >=
+    ROVNASA,            // ==
+    NEROVNASA,          // !=
+    LZATVORKA,          // (
+    PZATVORKA,          // )
+    ROVNA_SA,           // =
+    PICNA_ZATVORKA_L,   // {
+    PICNA_ZATVORKA_R,   // }
+    CIARKA              // ,
+}operators;
+
+typedef enum{
+    INTEGER=50,
+    DOUBLE,
+    STRING,
+}numbers;
+
+typedef enum{
+
+}states;
+
+int getNextToken();
 
 
 #endif //IFJ_88_SCANNER_H
