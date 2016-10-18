b9776b9fa894dbe4b5adae5a9c4019b1d4e9bef7
felhunter64 <ssulaj.martin@gmail.com>
2016-10-18 14:45:20 +0200

    mato

--- vyrazy.h
+++ vyrazy.h
@@ -9,29 +9,50 @@
 
 
 typedef enum{
-    PLUS,               // +
-    MINUS,              // -
-    KRAT,               // *
-    DELENO,             // /
-    MENSITKO,           // <
-    VACSITKO,           // >
-    MENSITKO_ROVNASA,   // <=
-    VACSITKO_ROVNASA,   // >=
-    ROVNASA,            // ==
-    NEROVNASA,          // !=
-    LZATVORKA,          // (
-    PZATVORKA,          // )
-    ID,                 // identifikator(premenna, funkcia, objekt(premenna), objekt(funkcia), literal)
-    DOLAR,              // koniec
-    CIARKA              // ,
-}enumVyrazy;
+    PLUS,              // +
+    MINUS,             // -
+    MULTIPLY,          // *
+    DIVIDE,            // /
+    LESS,              // <
+    GREATER,           // >
+    LESS_EQUAL,        // <=
+    GREATER_EQUAL,     // >=
+    EQUAL,             // ==
+    NOT_EQUAL,         // !=
+    L_BRACKET,         // (
+    R_BRACKET,         // )
+    ID,                // identifikator(premenna, funkcia, objekt(premenna), objekt(funkcia), literal)
+    END,               // koniec
+    COMMA              // ,
+}enumExpressions;
 
 typedef enum{
     GRE,    // >
     LES,    // <
     EQU,    // ==
     BAD     // syntakticka chyba
-}enumRovnost;
+}enumEquality;
+
+typedef enum{
+    RULE_ASSIGN,    // = Pouzit tu?
+    RULE_RETURN,    // return Pouzit tu?
+    RULE_IF,        // if Pouzit tu?
+
+    RULE_FUNC,      // zacina pouzitie nejakej funkcie - foo()
+    RULE_VAR,       // pouzitie premennej
+
+    RULE_PLUS,               // +
+    RULE_MINUS,              // -
+    RULE_MULTIPLY,           // *
+    RULE_DIVIDE,             // /
+    RULE_LESS,               // <
+    RULE_GREATER,            // >
+    RULE_LESS_EQUAL,         // <=
+    RULE_GREATER_EQUAL,      // >=
+    RULE_EQUAL,              // ==
+    RULE_NOT_EQUAL,          // !=
+    RULE_END                 // koniec
+}expressionRules;
 
 typedef struct sToken {
     char *stringToken;
